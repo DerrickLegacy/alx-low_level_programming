d748cbc597aff06b6bb45cfe3a22a0f33cee9ead
DerrickLegacy <ahaabwederrick67@gmail.com>
2023-02-22 12:36:26 -0800

    void print_alphabet_x10(void)

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,1 +1,19 @@
-#
+#include "main.h"
+
+/*
+ * print_alphabet_x10-prints a-z 10 times
+ */
+void print_alphabet_x10(void)
+{
+	char x;
+	int i;
+
+	for (i = 0 ; i < 10 ; i++)
+	{
+		for (x = 'a' ; x <= 'z' ; x++)
+		{
+			_putchar(x);
+		}
+		_putchar('\n');
+	}
+}
