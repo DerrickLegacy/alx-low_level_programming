086d82dd5724687df29c56f541d843a759463bb8
DerrickLegacy <ahaabwederrick67@gmail.com>
2023-02-23 02:24:17 -0800

    11. 98 Battery Street, the OG

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - print from specified n value to 98.
+ * @n: the value from which to print
+ */
+void print_to_98(int n)
+{
+	int i;
+
+	if (n > 98)
+	{
+		for (i = n ; i >= 98 ; i--)
+		{
+			printf("%d, ", i);
+		}
+		printf("\n");
+	}
+	if (n < 98)
+	{
+		for (i = n ; i <= 98 ; i++)
+		{
+			printf("%d, ", i);
+		}
+		printf("\n");
+	}
+}
