7dc8fe2be7f407a3460f33c7454de5ba739a1085
DerrickLegacy <ahaabwederrick67@gmail.com>
2023-02-23 07:24:18 -0800

    prints a line of length n

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_line - prints lines using the _
+ * @n: The number of times to print the line
+ *
+ * Return: Lines
+ */
+void print_line(int n)
+{
+	int i;
+
+	if ((n == 0) || (n <= 0))
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0 ; i <= n ; i++)
+		{
+			_putchar('_');
+		}
+		_putchar('$');
+	}
+	_putchar('\n');
+
+}
