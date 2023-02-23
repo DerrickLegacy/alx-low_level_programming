83f3cc706549a70c8a29cd5836672fb4b01dfaef
DerrickLegacy <ahaabwederrick67@gmail.com>
2023-02-23 07:30:52 -0800

    prints a line of length n

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -10,18 +10,17 @@ void print_line(int n)
 {
 	int i;
 
-	if ((n == 0) || (n <= 0))
-	{
-		_putchar('\n');
-	}
-	else
+	if ((n > 0))
 	{
 		for (i = 0 ; i <= n ; i++)
 		{
 			_putchar('_');
 		}
 		_putchar('$');
+		_putchar('\n');
 	}
-	_putchar('\n');
-
+	else
+	{
+		_putchar('\n');
+	}
 }
