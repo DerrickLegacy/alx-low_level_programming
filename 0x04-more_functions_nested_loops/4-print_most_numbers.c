fcae4ba0900bd171ab188feb52b0c97e4424e83f
DerrickLegacy <ahaabwederrick67@gmail.com>
2023-02-23 06:50:04 -0800

    Prints from 1-9 exclusive of 2 and 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -11,8 +11,10 @@ void print_most_numbers(void)
 
 	for (i = 0 ; i <= 9 ; i++)
 	{
-		if ((i != 2) && (i != 4)
-		_putchar((1 % 10) + '0');
+		if ((i != 2) && (i != 4))
+		{
+			_putchar((1 % 10) + '0');
+		}
 	}
 	_putchar('\n');
 }
