18b9c56812107497ec1dd1864950be9ea7765750
DerrickLegacy <ahaabwederrick67@gmail.com>
2023-02-24 02:18:35 -0800

    Checks for mumltiples of 3 and 5 and for both.

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,22 +11,26 @@ int main(void)
 	int i;
 
 	for (i = 1 ; i <= 100 ; i++)
-	{	if ((i % 3) && (i % 5))
+	{
+		if ((i % 3) && (i % 5))
 		{
-			printf("FizzBuzz ");
+			printf("FizzBuzz");
 		}
 		if ((i % 3) == 0)
 		{
-			printf("Fizz ");	
+			printf("Fizz");
 		}
 		if ((i % 5) == 0)
 		{
-			printf("Buzz ";
+			printf("Buzz";
 		}
-		else 
+		else
 		{
-			printf("%d ", i);
+			printf("%d", i);
 		}
+		if (i == 100)
+			continue;
+		printf(" ");
 	}
 	printf("\n");
 	return (0);
