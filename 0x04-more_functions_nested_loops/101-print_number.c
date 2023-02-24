e9f11ed2ed7feb72e9b8748e355dc7ac70fe00c2
DerrickLegacy <ahaabwederrick67@gmail.com>
2023-02-24 05:00:56 -0800

    Prints integers

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**print_number - function to print the integers
+ * @n: the value to be printed.
+ */
+ void print_number(int n)
+{
+	unsigned int x = n;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		x = -x;
+	}
+	if (x / 10) > 0)
+	{
+		print_number(x / 10);
+	}
+	_putchar((x % 10) + '0');
+}
