47547baa64ed3d9dece76accc17a4e8801e5237a
DerrickLegacy <ahaabwederrick67@gmail.com>
2023-02-28 13:39:47 -0800

    converts a string to an int

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _atoi - converts a string to an int
+ * @s: input to the function to be converted to int
+ * Return: Returns a generated number with a sign.
+ */
+int _atoi(char *s)
+{
+	int sign = 1;
+	unsigned int number = 0;
+
+	do {
+		if (*s == '-')
+			sign *= -1;
+
+		else if (*s >= '0' && *s <= '9')
+			number = (number * 10) + (*s - '0');
+
+		else if (number > 0)
+			break;
+
+	} while (*s++);
+
+	return (number * sign);
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,6 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
-
+#include <stdlib.h>
 /**
  * File_name: main.h
  * Auth: DerrickLegacy
@@ -8,6 +8,7 @@
  */
 
 int _putchar(char c);
+int _atoi(char *s);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
