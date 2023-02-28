d7ab05977a0cae4303faa42b660dc7589c92fc8c
DerrickLegacy <ahaabwederrick67@gmail.com>
2023-02-28 11:50:16 -0800

    Prints even chars

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * puts2 - Prints even characters
+ * @str: Input to the function
+ */
+void puts2(char *str)
+{
+	int i;
+	int length;
+
+	for (i = 0 ; str[i] != '\0' ; i++)
+	{
+		length++;
+	}
+	for (i = 0 ; i < length ; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
