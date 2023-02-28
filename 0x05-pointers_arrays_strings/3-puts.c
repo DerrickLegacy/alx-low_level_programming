df79eb1cf55cccdb44eee9487b78d5098e8f2de7
DerrickLegacy <ahaabwederrick67@gmail.com>
2023-02-28 05:09:02 -0800

    Prints a string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts - Prints the string literal passed in.
+ * @str: The input to our function.
+ * Return: Returns the printed string .
+ */
+
+void _puts(char *str)
+{
+	puts(str);
+}
