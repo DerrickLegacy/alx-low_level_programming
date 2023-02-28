19008362fdf8d9b6aa62b78e8c072541353624ba
DerrickLegacy <ahaabwederrick67@gmail.com>
2023-02-28 01:59:44 -0800

    First commit

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * reset_to_98(int *n) - takes a pointer to an int updates it to 98.
+ * reset_to_98 - takes a pointer to an int updates it to 98.
  * @n: value to be input.
  */
 void reset_to_98(int *n)
