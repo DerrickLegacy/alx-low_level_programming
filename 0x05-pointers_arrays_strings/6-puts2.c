573d81f59f2dee81a7e7241d590588324a3abd05
DerrickLegacy <ahaabwederrick67@gmail.com>
2023-02-28 12:48:02 -0800

    Prints even characters

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -4,16 +4,16 @@
  * puts2 - Prints even characters
  * @str: Input to the function
  */
+
 void puts2(char *str)
-{
-	int i;
+{	int i;
 	int length;
 
 	for (i = 0 ; str[i] != '\0' ; i++)
 	{
 		length++;
 	}
-	for (i = 0 ; i < length ; i++)
+	for (i = 0 ; i < length ; i += 2)
 	{
 		_putchar(str[i]);
 	}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -7,6 +7,7 @@
  * Desc: Contains all function prototypes fothis project 0x04.
  */
 
+int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
