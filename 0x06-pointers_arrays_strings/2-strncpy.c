53ba00da584882d2f6c53a4c3538b8421c1cea3c
DerrickLegacy <ahaabwederrick67@gmail.com>
2023-03-02 11:43:21 -0800

    Copying items using strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * _strncpy - Joins two words or sentences together.
+ * @dest: Vairable input to which a word or sentence is joined to.
+ * @n:  Maximum number of character to stop at of the @src.
+ * @src: Variable input to our function to be joined to another.
+ * Return: Always 0
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int index = 0;
+	int src_length = 0;
+
+	while (src[index++] != '\0')
+	{
+		src_length++;
+	}
+	for (index = 0; index < src_length ; index++)
+	{
+		if (index < n)
+		{
+			dest[index] = src[index];
+		}
+		else
+		{
+			dest[src_length] = '\0';
+			break;
+		}
+	}
+	return (dest);	
+}
