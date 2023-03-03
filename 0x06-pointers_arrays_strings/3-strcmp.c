c48e6d8632c2347c1bbeb967b6515cd4d3cd4c85
DerrickLegacy <ahaabwederrick67@gmail.com>
2023-03-03 08:50:20 -0800

    using th strcmp command mannually

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,7 +8,7 @@
  */
 int _strcmp(char *s1, char *s2)
 {	
-	while (*s1 != '\0')
+	while (*s1 != '\0' && *s2 != '\0')
 	{
 		if (*s1 == *s2)
 		{
@@ -23,5 +23,4 @@ int _strcmp(char *s1, char *s2)
 			return (-15);
 		}
 	}
-	return (0);
 }
