9fbd9cb63992a1a20690e7af11ea2c819a140320
DerrickLegacy <ahaabwederrick67@gmail.com>
2023-03-07 02:10:00 -0800

    the strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strchr - returns all strings after a matched character matched.
+ * @s: the source array from which to make a search.
+ * @c: the character to match.
+ * Return: returns the array containing thne matched character.
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		s++;
+	}
+	return (NULL);
+}
