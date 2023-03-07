59586fc5bb326c5c22c3cb8fb2574320b1be6773
DerrickLegacy <ahaabwederrick67@gmail.com>
2023-03-07 12:22:05 -0800

    The strstr function

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,29 @@
+#include  "main.h"
+
+/**
+ * _strstr - finds the first occurence of a substring
+ * @haystack: pointer whose strings are to be compared with @needle
+ * @needle: The other point whose strings are to be compared for a match.
+ * Return: returns the identified string plus the other parts.
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int j;
+
+	while (*haystack != '\0')
+	{
+		if (*haystack == needle[0])
+		{
+			for (j = 0 ; needle[j] != '\0' ; j++)
+			{
+				if (*(haystack + j) != needle[j])
+				{
+					break;
+				}
+			}
+			return (haystack);
+		}
+		haystack++;
+	}
+	return (NULL);
+}
