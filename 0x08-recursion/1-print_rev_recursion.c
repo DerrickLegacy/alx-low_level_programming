14fe2bee0c8b9c4ab73e205d30dca4c1528a42e5
DerrickLegacy <ahaabwederrick67@gmail.com>
2023-03-09 00:57:22 -0800

    reverse a string

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - Prints the reverse string
+ * @s: the input to our function
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s)
+	{
+		/* Move to the last eleent */
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
