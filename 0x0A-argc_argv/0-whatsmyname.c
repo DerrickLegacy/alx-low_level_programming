09e42d1393c34b9538ee8d15c236d5dd34b87293
DerrickLegacy <ahaabwederrick67@gmail.com>
2023-03-14 07:51:43 -0700

    Printing program name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - the major function to run our program.
+ * @argc: The size of the array containing the values of flags passed.
+ * @argv: the array containing the passed arguments or flags.
+ * Return: return zero when successful and an error otherwise
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s", argv[0]);
+	printf("\n");
+	return (0);
+}
