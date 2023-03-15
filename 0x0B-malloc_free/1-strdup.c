c1be30b115bbe79471ff11922311e9b17eeff59a
DerrickLegacy <ahaabwederrick67@gmail.com>
2023-03-15 05:25:25 -0700

    Makes a duplicate to a string

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <Stlib.h>
+
+/**
+ * _strdup - Duplicates a string
+ * @str: the array to be duplicated
+ * Return: Null if unsuccessful,else an array pointer
+ */
+char *_strdup(char *str)
+{
+	int len = 1;
+	int j;
+	char *dup;
+
+	if (str == NULL)
+		return (NULL);
+	/* Obtail length of the original string */
+	while (str[i])
+	{
+		len++;
+	}
+	dup = malloc((sizeof(char) * len) + 1);
+	/* validate malloc if NULL */
+	if (dup == NULL)
+		return (NULL);
+	/* Copying from str too dup */
+	for (j = 0 ; j < len ; j++)
+	{
+		dup[j] = str[j];
+	}
+	dup[len] = '\0';
+	return (dup);
+}
