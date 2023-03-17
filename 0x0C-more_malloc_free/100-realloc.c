953d502c40572653c644c00065bacd7d63b47131
DerrickLegacy <ahaabwederrick67@gmail.com>
2023-03-17 13:29:40 -0700

    Reallocate memory

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -13,27 +13,21 @@
 
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	void *new_ptr;
-
-	if (new_size == 0)
+	if (new_size == 0 && ptr != NULL)
 	{
 		free(ptr);
 		return (NULL);
 	}
 	if (ptr == NULL)
 	{
-		return (malloc(new_size));
+		ptr = malloc(new_size);
 	}
-	if (new_size <= old_size)
+	if (new_size == old_size)
 	{
 		return (ptr);
 	}
-	new_ptr = malloc(new_size);
-	if (new_ptr == NULL)
-	{
-		return (NULL);
-	}
-
-	memcpy(new_ptr, ptr, old_size);
 	free(ptr);
+	ptr = malloc(new_size)
+
+	return (ptr);
 }
